1000c568d329ee502db49da6bea16612f2d19b5c
zackwu <zack.wu@neurobraindynamics.com>
2025-11-25 10:18:28 +0800

    route

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+app.get("/",function(req,res){
+    res.send("Hello home page");
+});
+app.get("/getData",function(req,res){
+    res.send("這是就是你要的資料");
+});
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
